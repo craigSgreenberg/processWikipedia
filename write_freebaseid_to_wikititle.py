37c1e4c6ab7535b10765d0f535411817ec84a866
cSg <csgreenberg@cs.umass.edu>
2015-04-26 13:54:56 -0400

    added script to transform the wikipedia link_path to (actual) wikipedia page titles.

---
+++ write_freebaseid_to_wikititle.py
@@ -0,0 +1,33 @@
+
+def relpath_to_title(relpath, prefix='/wikipedia/en_title/', code_length=5):
+    assert relpath.startswith(prefix)
+    relpath = relpath[len(prefix):]
+    relpath = relpath.replace("_", " ")
+    # encodes unicode as $XXXX with XXXX being the hex unicode codepoint
+    bad_index = relpath.find('$')
+    while bad_index != -1:
+        # +1 to skip the starting $
+        hex_unicode_point = relpath[bad_index+1:bad_index+code_length]
+        replace_char = unichr(int(hex_unicode_point, 16)).encode('utf-8')
+        relpath = relpath[:bad_index] + replace_char + relpath[bad_index+code_length:]
+        bad_index = relpath.find('$')
+    return relpath
+
+def write_freebaseid_to_wikititle(infilepath, outfilepath, sep='\t'):
+    with open(outfilepath, 'w') as outf:
+        with open(infilepath) as inf:
+            for line in inf:
+                line = line.strip()
+                if not line:
+                    continue
+                freebaseid, _, relpath = line.split(sep)
+                title = relpath_to_title(relpath)
+                print title
+
+def main():
+    infilepath = '/iesl/canvas/proj/processedClueweb12/freebase/msr/freebaseid_2_wikipedia-train_only.tsv'
+    outfilepath = '/iesl/canvas/proj/processedClueweb12/freebase/msr/freebaseid_2_wikititle-train_only.tsv'
+    write_freebaseid_to_wikititle(infilepath, outfilepath)
+
+if __name__ == '__main__':
+    main()
