0162050c8745833be9ff38a88295e8511e0ea122
cSg <csgreenberg@cs.umass.edu>
2015-04-27 19:11:49 -0400

    Added script to compute idfs.

---
+++ compute_idf.py
@@ -0,0 +1,25 @@
+
+from __future__ import division
+from math import  log
+
+def write_idfs(infilepath, outfilepath, num_docs_in_corpus=2213898, sep='\t'):
+    with open(outfilepath) as outfile:
+        with open(infilepath) as infile:
+            for line in infile:
+                line = line.strip()
+                if not line:
+                    continue
+                ## assume all tokens contain no spaces
+                num_docs_containing_token, token = line.split()
+                num_docs_containing_token = int(num_docs_containing_token)
+                ## default log is e, we're choosing log base 2
+                idf = log(num_docs_in_corpus/num_docs_containing_token, 2)
+                outfile.write(token+sep+idf+'\n')
+
+
+def main():
+    infilepath = '/iesl/canvas/proj/processedClueweb12/enwiki-20150304-pages-articles_freebase-train-only_dfs.txt'
+    outfilepath = '/iesl/canvas/proj/processedClueweb12/enwiki-20150304-pages-articles_freebase-train-only_idfs.txt'
+    write_idfs(infilepath, outfilepath)
+if __name__ == '__main__':
+    main()
