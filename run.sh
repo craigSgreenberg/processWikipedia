1b798076d024df80198dfa0c16896ff54f7223e8
cSg <csgreenberg@cs.umass.edu>
2015-04-26 20:43:03 -0400

    debug

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ fi
 
 classpath=`cat CP.hack`
 app="java -Xmx30g -cp ${classpath} cc.factorie.app.nlp.load.LoadWikipediaPlainText"
-$app 1000 /iesl/canvas/proj/processedClueweb12/wikipedia/enwiki/enwiki-20150304-pages-articles.xml.bz2
+$app 1000 /iesl/canvas/proj/processedClueweb12/wikipedia/enwiki/raw/enwiki-20150304-pages-articles.xml.bz2
 #--train=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseTrain.v0.tsv --dev=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseDev70.v0.tsv --test=/iesl/canvas/proj/processedClueweb12/freebase/msr/msrFreebaseTest70.v0.tsv --testRelationsFile=/iesl/canvas/proj/processedClueweb12/freebase/msr/release_v0.4/70Types --size $1 --rate $2 --regularizer $3 --negative $4 --threads $5 --options 3  --epochs 30 --frequency 10 --tree $6
